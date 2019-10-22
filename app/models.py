f95c3e569bdaf3653653123b62ded3a22c96e2db
jecinta <jecintawanjirug@gmail.com>
2019-10-22 13:13:34 +0300

    this adds the db Column of username,firstname,lastname,and email.

--- app/models.py
+++ app/models.py
@@ -6,9 +6,18 @@ from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
 
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index =True)
+    firstname = db.Column(db.String(255))
+    lastname = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(5000))
+    profile_pic_path = db.Column(db.String)
+    pass_secure = db.Column(db.String(255))
+    date_joined = db.Column(db.DateTime,default=datetime.utcnow)
+
+    
 def __repr__(self):
     return f'User {self.username}'
 
