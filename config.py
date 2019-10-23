f92c56cb5fce87052d0ec4f648b0b4d04da7d692
jecinta <jecintawanjirug@gmail.com>
2019-10-23 18:08:46 +0300

    this adds a secret key to the config.py file

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config:
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jecinta:twinkle@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static'
+    SECRET_KEY = '69df8b1b7481455e9e27bb627a473aaf'
+
     # email configurations
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
@@ -14,6 +16,7 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
+    DEBUG = True
 
 class ProdConfig(Config):
     '''
@@ -22,8 +25,9 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+    pass
 class TestConfig(Config):
     '''
     Testing configuration child class
@@ -32,6 +36,7 @@ class TestConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jecinta:twinkle@localhost/pitch'
+    pass
 
 class DevConfig(Config):
     '''
@@ -41,9 +46,9 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jecinta:twinkle@localhost/pitch'
-
-    DEBUG = True
-    ENV = 'development'
+    pass
+   
+    #ENV = 'development'
     
 config_options = {
 'development':DevConfig,
