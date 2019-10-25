9c68128a7c3c09b7db326d41ae581df923bc0349
jecinta <jecintawanjirug@gmail.com>
2019-10-25 10:25:05 +0300

    deploy app

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export MAIL_USERNAME='jecintawanjirug@gmail.com'
 export MAIL_PASSWORD='0770534521'
 export SECRET_KEY='69df8b1b7481455e9e27bb627a473aaf'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jecinta:twinkle@localhost/pitch'
 
 python3.6 manage.py server
